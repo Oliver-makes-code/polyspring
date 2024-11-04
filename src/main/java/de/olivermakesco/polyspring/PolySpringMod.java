f4e3901966c85ee04b6ed76a20e38810b236b15f
Oliver-makes-code <oliver@olivermakesco.de>
2024-11-04 15:50:32 -0600

    Init

---
+++ src/main/java/de/olivermakesco/polyspring/PolySpringMod.java
@@ -0,0 +1,10 @@
+package de.olivermakesco.polyspring;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PolySpringMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
